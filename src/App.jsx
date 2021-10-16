70e8eb78bb262467824326873c18754f722b82f8
Ali Naqi Khan <alinaqikofficial@gmail.com>
2021-10-16 23:19:44 +0500

    Implemented Login page, added functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './stylesheets/App.css';
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { Home, Login, Signup } from './pages';
 import * as ROUTES from './constants/routes';
 
@@ -8,20 +8,6 @@ export const App = () => {
   
   return (
     <Router>
-        <nav>
-          <ul>
-            <li>
-              <Link to={ROUTES.HOME}>Home</Link>
-            </li>
-            <li>
-              <Link to={ROUTES.LOGIN}>Login</Link>
-            </li>
-            <li>
-              <Link to={ROUTES.SIGN_UP}>Sign Up</Link>
-            </li>
-          </ul>
-        </nav>
-
         <Switch>
           <Route path={ROUTES.LOGIN} exact>
             <Login />
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,58 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { Link, useHistory } from "react-router-dom";
+import { auth } from '../libraries/firebase';
+import { signInWithEmailAndPassword, signInWithGoogle } from "../authentication";
+import '../stylesheets/Login.css';
 
 export const Login = () => {
-    return (
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [user, loading ] = useAuthState(auth);
+  const history = useHistory();
+
+  useEffect(() => {
+    if (loading) {
+      // trigger a loading screen
+      return;
+    }
+    if (user) history.replace("/dashboard");
+  }, [user, loading]);
+
+  return (
+    <div className="login">
+      <div className="login__container">
+        <input
+          type="text"
+          className="login__textBox"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="Email Address"
+        />
+        <input
+          type="password"
+          className="login__textBox"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Password"
+        />
+        <button
+          className="login__btn"
+          onClick={() => signInWithEmailAndPassword(email, password)}
+        >
+          Login
+        </button>
+        <button className="login_btn login_google" onClick={signInWithGoogle}>
+          Login with Google
+        </button>
         <div>
-            Login
+          <Link to="/reset">Forgot Password</Link>
         </div>
-    )
-}
+        <div>
+          Don&apos;t have an account? <Link to="/register">Register</Link> now.
+        </div>
+      </div>
+    </div>
+  );
+};
